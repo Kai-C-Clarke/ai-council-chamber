e7dcf6d1b58054ecdc049fe18a51793710f691af
Kai-C-Clarke <kai.chat.gemini@gmail.com>
2025-04-30 22:56:51 +0100

    Add files via upload

--- Enabling Solas – Council Chamber Phase.py
+++ Enabling Solas – Council Chamber Phase.py
@@ -74,4 +74,5 @@ def webhook():
     return jsonify(success=True, status="Message received from webhook")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
+
